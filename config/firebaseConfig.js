55b04ca93b72fe60120ef948003753619ef83e4d
Gareth Jones <garethjones@192.168.1.101>
2021-12-31 19:59:23 +0000

    firebase initial integration

---
+++ config/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuNyRwunUZpLvVHax8ld1gG-xsvDZi_38",
+  authDomain: "weather-inspector-eb6ac.firebaseapp.com",
+  projectId: "weather-inspector-eb6ac",
+  storageBucket: "weather-inspector-eb6ac.appspot.com",
+  messagingSenderId: "608782694000",
+  appId: "1:608782694000:web:a936998a6ecd051e5ba6b5"
+};
+
+export default firebaseConfig;
---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import firebaseConfig from "../config/firebaseConfig";
+
+// variable acts as an entry point between app and firebase
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+// firebase authentication
+const auth = getAuth(firebaseApp);
+
+// firebase db
+const db = getFirestore(firebaseApp);
+
+export { auth, db };
